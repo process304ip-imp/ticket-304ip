261a57d11fe2ae8d26f29f8488b9a520f0582d01
Copper Hodl <delta@Coppers-MacBook-Pro.local>
2026-05-12 10:18:18 +0700

    Add netlify.toml for standardized build and proxy config

--- template/dev-dist/sw.js
+++ template/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i3o7ns5230s"
+    "revision": "0.ad1a4fkre"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
